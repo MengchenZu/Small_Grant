9bda30fa646c196b0449e2890b12087a928ed72f
Mengchen(Herman) Zu <zmc748159263@gmail.com>
2017-11-22 02:52:35 +1100

    Update final_Southerly.R

--- final_Southerly/final_Southerly.R
+++ final_Southerly/final_Southerly.R
@@ -5,46 +5,10 @@ library("ggplot2")
 library("lubridate")
 library("stringr")
 
-my_read_dfr_metadata = function (filenames) 
-{
-  all_rows <- do.call(rbind, lapply(filenames, my_read_dfr_citations))
-  result <- unique(all_rows)
-  if (any(duplicated(result$id))) {
-    warning("Some rows have the same id")
-  }
-  result
-}
+data_dir <- file.path("data/Southerly")
 
-my_read_dfr_citations = function (filename, strip.white = TRUE, ...) 
-{
-  if (grepl("\\.tsv", filename, ignore.case = TRUE)) {
-    cols <- scan(filename, nlines = 1, what = character(), 
-                 sep = "\t", quiet = TRUE)
-    if (length(cols) != 13) {
-      warning("Expected 13 tab-delimited columns but found ", 
-              length(cols), "\nResults may not be valid")
-    }
-    result <- read.table(filename, header = FALSE, skip = 1, 
-                         sep = "\t", col.names = cols, quote = "", as.is = TRUE, 
-                         comment = "", strip.white = strip.white, fill= TRUE, ...)
-  }
-  else {
-    cols <- scan(filename, nlines = 1, what = character(), 
-                 sep = ",", quiet = TRUE)
-    result <- read.csv(filename, skip = 1, header = FALSE, 
-                       col.names = cols, quote = "", as.is = TRUE, comment = "", 
-                       strip.white = strip.white, fill= TRUE, ...)
-  }
-  result <- dplyr::tbl_df(result)
-  # result$pubdate <- pubdate_Date(result$pubdate)
-  result$type <- factor(result$type)
-  result
-}
-
-data_dir <- file.path("data/Single")
-
-metadata_file <- file.path("New_Southerly_part.tsv")
-meta <- my_read_dfr_metadata(metadata_file)
+metadata_file <- file.path("New_Southerly.tsv")
+meta <- read_dfr_metadata(metadata_file)
 
 counts<- NULL
 mydirs <- list.dirs(path = data_dir, full.names = TRUE)
@@ -61,16 +25,16 @@ counts <- counts %>% wordcounts_remove_stopwords(stoplist)
 
 ilist <- wordcounts_instances(counts)
 
-m <- train_model(ilist, n_topics=5,
+m <- train_model(ilist, n_topics=40,
                  n_iters=500,
                  seed=1066,       # "reproducibility"
                  metadata=meta    # optional but handy later
                  # many more parameters...
 )
 
-doc_ids(m)[!doc_ids(m) %in% meta$id]
-meta$id[!meta$id %in% doc_ids(m)]
-
 write_mallet_model(m, "modeling_results")
 
-dfr_browser(m)
+# metadata_file <- file.path("New_Southerly.tsv")
+# m <- load_mallet_model_directory("modeling_results", metadata_file=metadata_file)
+
+dfr_browser(m, "browser")
